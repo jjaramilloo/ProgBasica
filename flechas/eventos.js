248e6eca38362fe61e8f935fde62e939ba21f2b0
JJaramilloo <juliana.jaramillo@gmail.com>
2020-10-23 12:17:33 -0500

    Cambios hasta dibujar con flechas

--- flechas/eventos.js
+++ flechas/eventos.js
@@ -10,13 +10,44 @@ document.addEventListener("keyup", dibujarTeclado);
 console.log(teclas);
 var cuadrito = document.getElementById("area_dibujo");
 var papel = cuadrito.getContext("2d");
+var x= 150;
+var y=150;
 
 //dibujarLinea("red", 100, 100, 200, 200, papel);
-dibujarLinea("red", 149, 149, 151, 151, papel); //punto de inicio
+//dibujarLinea("red", 149, 149, 151, 151, papel); //punto de inicio de forma estatica
+dibujarLinea("black", x-1, y-1, x+1, y+1, papel); //punto de inicio de forma dinámica
 
 function dibujarTeclado(evento)
 {
-    //alert("prueba teclado");
+       var colorcito = "#4A148C";
+    var movimiento = 10;  
+    switch(evento.keyCode)
+    {
+        case teclas.LEFT:
+            //console.log("Va a la izquierda");            
+            dibujarLinea(colorcito, x, y, x-movimiento, y, papel);
+            x = x - movimiento;
+        break;
+        case teclas.UP:
+            //console.log("Va arriba");
+            dibujarLinea(colorcito, x, y, x, y-movimiento, papel);
+            y = y - movimiento;
+        break;
+        case teclas.RIGHT:
+            //console.log("Va a la derecha");
+            dibujarLinea(colorcito, x, y, x+movimiento, y, papel);
+            x = x + movimiento;
+        break;
+        case teclas.DOWN: 
+           //console.log("Va abajo");
+           dibujarLinea(colorcito, x, y, x, y+movimiento, papel);
+           y = y + movimiento;
+        break;
+        default:
+            console.log("Otra tecla");
+        break;
+    }
+     //alert("prueba teclado");
     //console.log("Tecla presionada");
     //console.log(evento.keyCode);
     //if (evento.keyCode == teclas.LEFT)
@@ -26,25 +57,7 @@ function dibujarTeclado(evento)
     //if (evento.keyCode == teclas.UP)
     //{
      //   console.log("Va arriba");
-    //}  
-    switch(evento.keyCode)
-    {
-        case teclas.LEFT:
-            console.log("Va a la izquierda");
-        break;
-        case teclas.UP:
-            console.log("Va arriba");
-        break;
-        case teclas.RIGHT:
-            console.log("Va a la derecha");
-        break;
-        case teclas.DOWN: 
-           console.log("Va abajo");
-        break;
-        default:
-            console.log("Otra tecla");
-        break;
-    }
+    //}
 }
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
