eab524dba9d2fb86b8c9f28088e3858a006f6f0f
JJaramilloo <juliana.jaramillo@gmail.com>
2020-11-04 17:23:48 -0500

    FizzBuzz y el inicio del juego pakiman

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -2,18 +2,20 @@
 var numeros = 100;
 for (var i=1; i <= 100; i++)
 {
-    if (i % 3 == 0)
+    if  ((i % 3 === 0) && (i % 5 === 0))
     {
-        document.write("fizz" + "<br />")
+        document.write("FizzBuzz" + "<br />")
     }
-    if (i % 5 == 0)
+    else if (i % 5 === 0)
     {
-        document.write("buzz" + "<br />")
+        document.write("Buzz" + "<br />")
+    }
+    else if (i % 3 === 0)
+    {
+        document.write("Fizz" + "<br />")
     }
     else
-    {
+        {
         document.write(i + "<br />")
     }
-
-    //document.write(i + "<br />");
 }
