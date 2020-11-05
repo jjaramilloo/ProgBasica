c1299ef389b4e4437d3e6fc734926cee4d0b93cf
JJaramilloo <juliana.jaramillo@gmail.com>
2020-11-04 19:29:11 -0500

    avance del juego pakiman

--- packiman/paki.js
+++ packiman/paki.js
@@ -1,1 +1,12 @@
-alert ("Pakiman");
+class Pakiman
+{
+    constructor(n)
+    {
+        this.nombre = n;
+        this.tipo = "tierra";
+    }
+}
+var cauchin = new Pakiman("Cauchin"); // es un objeto que viene de la misma intancia pakiman
+console.log(cauchin);
+var pokacho = new Pakiman("Pokacho");
+console.log(pokacho);
