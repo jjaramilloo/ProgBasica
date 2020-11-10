6c019f22145344752d96113eb00d72564c250d4e
JJaramilloo <juliana.jaramillo@gmail.com>
2020-11-10 16:20:36 -0500

    uso desde el servidor

---
+++ express/servidor.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var aplicacion = express();
+
+aplicacion.get("/", inicio);
+aplicacion.get("/cursos", cursos);
+
+function inicio(peticion, resultado)
+{
+    resultado.send("Esta es el <strong>home</strong>");
+}
+
+function cursos(peticion, resultado)
+{
+    resultado.send("Estos son los <strong>cursos</strong>");
+}
+aplicacion.listen(8990);
